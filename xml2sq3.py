2852d9a8d7ed66792da88da23798408daf571878
Rubizon <rubenroeffaers@hotmail.com>
2013-07-18 10:51:12 +0200

    Update xml2sq3.py

--- xml2sq3.py
+++ xml2sq3.py
@@ -5,7 +5,7 @@
 # description   convert a xml to a sq3 file
 # author        ruben roeffaers
 # requirements  sqlite3 installed
-# usage         python xml2sq3 xmlfile1 [ ,xmlfile2 [ ,xmlfile3 [,...] ] ]
+# usage         python xml2sq3.py xmlfile1 [ ,xmlfile2 [ ,xmlfile3 [,...] ] ]
 # outcome       *.xml2sq3.sq3 sqlite3 database file
 #
 # database description
